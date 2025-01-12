2d77af63e12dfa9c0a6be9c2868b3c3b910ea253
Levin Jeck <seternate@gmail.com>
2025-01-12 11:19:01 +0100

    Update bitwarden-updater service.

--- services/bitwarden-updater/config/update_bitwarden.sh
+++ services/bitwarden-updater/config/update_bitwarden.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 # Run updates
-#/bitwarden/updateself.sh 2>&1 > /dev/null
-#updateself_status=$?
-updateself_status=0
-#/bitwarden/update.sh 2>&1 > /dev/null
-#update_status=$?
-update_status=0
+/bitwarden/updateself.sh
+updateself_status=$?
+/bitwarden/update.sh
+update_status=$?
 
-#Write file for healthcheck
+#Write file for docker healthcheck
 status=1
-if [ $updateself_status -eq 0 and $update_status -eq 0 ]; then
+if [ $updateself_status -eq 0 ] && [ $update_status -eq 0 ]; then
     status=0
-else
+fi
 printf "%d" $status > /health
 
 #Write metrics to file for prometheus
-printf "bitwarden_updater_updateself_status %d" $updateself_status > /prometheus/bitwarden_updater.prom
-printf "bitwarden_updater_update_status %d" $update_status >> /prometheus/bitwarden_updater.prom
+printf "bitwarden_updater_updateself_status %d\n" $updateself_status > /prometheus/bitwarden_updater.prom
+printf "bitwarden_updater_update_status %d\n" $update_status >> /prometheus/bitwarden_updater.prom
