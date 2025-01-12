76c3de5045c1c5736c9e0ff0f585a27041b0effb
Levin Jeck <seternate@gmail.com>
2025-01-12 15:51:20 +0100

    Add docker-image-pruner service.

---
+++ services/docker-image-pruner/config/prune_images.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Prune all unused images
+docker image prune --all --force
+image_prune_status=$?
+
+#Write file for docker healthcheck
+printf "%d" $image_prune_status > /health
+
+#Write metrics to file for prometheus
+printf "# HELP docker_image_pruner_status Pruning status: 0=success, 1>=fail\n" > /prometheus/docker_image_pruner.prom
+printf "# TYPE docker_image_pruner_status gauge\n" >> /prometheus/docker_image_pruner.prom
+printf "docker_image_pruner_status %d\n" $image_prune_status >> /prometheus/docker_image_pruner.prom
