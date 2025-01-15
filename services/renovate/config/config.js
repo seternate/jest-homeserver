fc583f18995bb3ff1240a8a90b29f5b5eb8a7278
Levin Jeck <seternate@gmail.com>
2025-01-15 21:00:57 +0100

    Update renovate.

--- services/renovate/config/config.js
+++ services/renovate/config/config.js
@@ -1,5 +1,7 @@
-module.exports = {
+module.exports = function () {
+  return {
     token: fs.readFileSync("/run/secrets/renovate_token"),
     repositories: ["seternate/jest-homeserver"],
     dryRun: "full",
+  }
 };
