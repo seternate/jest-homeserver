4bb1de8c5be7b3f113f90f55a746b5fb8f78f9aa
Levin Jeck <seternate@gmail.com>
2025-01-15 16:29:15 +0100

    Update renovate.

--- services/renovate/config/config.js
+++ services/renovate/config/config.js
@@ -1,5 +1,5 @@
-module.export = {
+module.exports = {
     token: fs.readFileSync("/run/secrets/renovate_token"),
     repositories: ["seternate/jest-homeserver"],
     dryRun: "full",
-}
+};
--- services/renovate/config/renovate.sh
+++ services/renovate/config/renovate.sh
@@ -2,6 +2,7 @@
 
 docker compose up --abort-on-container-failure renovate
 renovate_status=$?
+docker compose down renovate
 
 #Write metrics to file for prometheus
 printf "# HELP renovate_status Renovate status: 0=success, 1>=fail\n" > /prometheus/renovate.prom
