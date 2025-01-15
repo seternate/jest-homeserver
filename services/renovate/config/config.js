9a183ecd0b50c32cb64ab713d298516d95182447
Levin Jeck <seternate@gmail.com>
2025-01-15 15:33:15 +0100

    Add cron script.

--- services/renovate/config/config.js
+++ services/renovate/config/config.js
@@ -1,4 +1,5 @@
 module.export = {
     token: fs.readFileSync("/run/secrets/renovate_token"),
     repositories: ["seternate/jest-homeserver"],
+    dryRun: "full",
 }
---
+++ services/renovate/config/renovate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker compose up --abort-on-container-failure renovate
+renovate_status=$?
+
+#Write metrics to file for prometheus
+printf "# HELP renovate_status Renovate status: 0=success, 1>=fail\n" > /prometheus/renovate.prom
+printf "# TYPE renovate_status gauge\n" >> /prometheus/renovate.prom
+printf "renovate_status %d\n" $renovate_status >> /prometheus/renovate.prom
