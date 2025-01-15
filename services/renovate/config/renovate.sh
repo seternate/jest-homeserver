2b6386c274ab7b5712d2407dc1310835b1527db5
Levin Jeck <seternate@gmail.com>
2025-01-15 21:33:38 +0100

    Update renovate.

--- services/renovate/config/renovate.sh
+++ services/renovate/config/renovate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker compose --file /docker/docker-compose.yaml up --abort-on-container-failure renovate
+docker compose --file ${DOCKER_COMPOSE_PATH}/docker-compose.yaml up --abort-on-container-failure renovate
 renovate_status=$?
-docker compose --file /docker/docker-compose.yaml down renovate
+docker compose --file ${DOCKER_COMPOSE_PATH}/docker-compose.yaml down renovate
 
 #Write metrics to file for prometheus
 printf "# HELP renovate_status Renovate status: 0=success, 1>=fail\n" > /prometheus/renovate.prom
